8248b32386d4da4cb786d72d86a9130143f8ad33
spdevuk@gmail.com <spdevuk@gmail.com>
2019-08-15 13:31:15 +0100

    feat: make background image work with image or video

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './hero.scss';
+
+const imageRegex = /\.(gif|jpg|jpeg|tiff|png)$/i;
 
 const Hero = ({ background }) => {
   const [videoLoaded, setVideoloaded] = useState(false);
   return (
     <section className="hero is-dark is-fullheight">
-      <video
-        onCanPlay={() => setVideoloaded(true)}
-        style={{ display: videoLoaded ? 'block' : 'none' }}
-        preload="none"
-        autoPlay
-        controls
-        muted
-        loop
-        id={background}
-      >
-        <source src={background} type="video/mp4" />
-      </video>
+      {background.match(imageRegex) ? (
+        <img src={background} alt="hero background" />
+      ) : (
+        <video
+          onCanPlay={() => setVideoloaded(true)}
+          style={{ display: videoLoaded ? 'block' : 'none' }}
+          autoPlay
+          muted
+          loop
+          id={background}
+        >
+          <source src={background} type="video/mp4" />
+        </video>
+      )}
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">Fullheight title</h1>
-          <h2 className="subtitle">Fullheight subtitle</h2>
+          {/* <h1 className="title">Fullheight title</h1> */}
+          {/* <h2 className="subtitle">Fullheight subtitle</h2> */}
         </div>
       </div>
     </section>
   );
+};
+Hero.propTypes = {
+  background: PropTypes.string,
 };
 
 export default Hero;
